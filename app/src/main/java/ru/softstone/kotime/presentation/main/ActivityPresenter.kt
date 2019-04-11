3a36b632b68fff2885031e35225195488b8349a6
Aleksey Korobeynikov <uxeb@ya.ru>
2019-04-11 15:12:04 +0400

    доработка стилей диалогов

--- app/src/main/java/ru/softstone/kotime/presentation/main/ActivityPresenter.kt
+++ app/src/main/java/ru/softstone/kotime/presentation/main/ActivityPresenter.kt
@@ -2,15 +2,17 @@ package ru.softstone.kotime.presentation.main
 
 import com.arellomobile.mvp.InjectViewState
 import ru.softstone.kotime.architecture.presentation.BasePresenter
-import ru.softstone.kotime.presentation.*
+import ru.softstone.kotime.presentation.ACTIONS_SCREEN
+import ru.softstone.kotime.presentation.CATEGORIES_SCREEN
+import ru.softstone.kotime.presentation.STAT_SCREEN
+import ru.softstone.kotime.presentation.TIMER_SCREEN
 import ru.terrakok.cicerone.Router
 import javax.inject.Inject
 
 @InjectViewState
 class ActivityPresenter @Inject constructor(private val router: Router) : BasePresenter<ActivityView>() {
     fun onFirstCreate() {
-//        router.newRootScreen(TIMER_SCREEN)
-        router.newRootScreen(ERROR_SCREEN)
+        router.newRootScreen(TIMER_SCREEN)
     }
 
     fun onShowTimerClick() {
